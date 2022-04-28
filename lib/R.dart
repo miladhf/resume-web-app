69e3ef62276c74fb87cc6b691bf53bf2fb497519
miladhf <40568123+miladhf@users.noreply.github.com>
2022-04-28 19:56:34 +0430

    simple R class added

---
+++ lib/R.dart
@@ -0,0 +1,25 @@
+class R {
+  static final icons = _Icons();
+  static final images = _Images();
+  static final strings = _Strings();
+  static final animations = _Animations();
+  static final size = _Sizes();
+}
+
+class _Strings {}
+
+class _Icons {
+  // final ic_profile = 'assets/icons/ic_profile.svg';
+}
+
+class _Images {
+  // final monitor = 'assets/images/monitor_image.png';
+}
+
+class _Animations {
+  // final remove = 'assets/animations/remove.json';
+}
+
+class _Sizes {
+  // final double textFieldLowWidth = 250;
+}
