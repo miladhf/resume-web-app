5f06ec5f4edc6c43800723a523912825bee2ae71
miladhf <miladhasel@gmail.com>
2024-01-03 01:06:22 +0330

    home tab animation completed

--- lib/pages/main_tabs/home_tab/home_tab.dart
+++ lib/pages/main_tabs/home_tab/home_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:resume_web/utils/utils.dart';
 import 'package:resume_web/widgets/home/home_buttons.dart';
 import 'package:resume_web/widgets/home/home_icons_view.dart';
@@ -23,7 +24,7 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-  _firstRan() {
+  _firstRun() {
     HomeTab.isFirstRun = false;
   }
 
@@ -31,7 +32,6 @@ class _HomeTabState extends State<HomeTab> {
   Widget build(BuildContext context) {
     var isDesktop = Utils.isDesktop(context);
     double height = 175;
-
     return Stack(
       alignment: Alignment.center,
       children: [
@@ -42,55 +42,73 @@ class _HomeTabState extends State<HomeTab> {
           fit: BoxFit.cover,
         ),
         Positioned(
-          bottom: isDesktop ? 150 : 120,
-          right: Utils.isRtlLocale(context) ? (isDesktop ? 70 : 30) : null,
-          left: Utils.isRtlLocale(context) ? null : (isDesktop ? 70 : 30),
-          child: SizedBox(
-            height: height,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const HomeIconsView(),
-                const SizedBox(
-                  width: 25,
-                ),
-                Container(
-                  width: 2,
+            bottom: isDesktop ? 150 : 120,
+            right: Utils.isRtlLocale(context) ? (isDesktop ? 70 : 30) : null,
+            left: Utils.isRtlLocale(context) ? null : (isDesktop ? 70 : 30),
+            child: Animate(
+                effects: HomeTab.isFirstRun
+                    ? [
+                        MoveEffect(
+                          begin: MoveEffect.neutralValue.copyWith(
+                              dx: MediaQuery.of(context).size.width + 500),
+                          duration: 1500.milliseconds,
+                        ),
+                        ShimmerEffect(
+                          duration: 1500.milliseconds,
+                          delay: 1500.milliseconds,
+                        ),
+                      ]
+                    : [],
+                onComplete: (c) {
+                  _firstRun();
+                },
+                child: SizedBox(
                   height: height,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-                const SizedBox(
-                  width: 25,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      UserData.getMyData().name,
-                      style: Theme.of(context).textTheme.headline1?.copyWith(
-                            fontSize: isDesktop ? 40 : 35,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const HomeIconsView(),
+                      const SizedBox(
+                        width: 25,
+                      ),
+                      Container(
+                        width: 2,
+                        height: height,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      const SizedBox(
+                        width: 25,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            UserData.getMyData().name,
+                            style:
+                                Theme.of(context).textTheme.headline1?.copyWith(
+                                      fontSize: isDesktop ? 40 : 35,
+                                    ),
                           ),
-                    ),
-                    Text(
-                      UserData.getMyData().jobTitle,
-                      style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                            fontSize: isDesktop ? 30 : 25,
+                          Text(
+                            UserData.getMyData().jobTitle,
+                            style:
+                                Theme.of(context).textTheme.bodyText1?.copyWith(
+                                      fontSize: isDesktop ? 30 : 25,
+                                    ),
                           ),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    HomeButtons(
-                      onAboutMeTap: widget.onAboutMeTap,
-                      onPortfolioTap: widget.onPortfolioTap,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          HomeButtons(
+                            onAboutMeTap: widget.onAboutMeTap,
+                            onPortfolioTap: widget.onPortfolioTap,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ))),
       ],
     );
   }
