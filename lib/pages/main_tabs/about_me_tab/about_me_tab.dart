839cf49b6975449ee103c8d959bd9ece61088580
miladhf <miladhasel@gmail.com>
2024-01-05 00:07:22 +0330

    about me tab animation completed

--- lib/pages/main_tabs/about_me_tab/about_me_tab.dart
+++ lib/pages/main_tabs/about_me_tab/about_me_tab.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:resume_web/utils/utils.dart';
 
 import '../../../data/user_data.dart';
@@ -16,7 +17,7 @@ class AboutMeTab extends StatefulWidget {
 }
 
 class _AboutMeTabState extends State<AboutMeTab> {
-  _firstRan() {
+  _firstRun() {
     AboutMeTab.isFirstRun = false;
   }
 
@@ -67,43 +68,63 @@ class _AboutMeTabState extends State<AboutMeTab> {
           right: isRtlLocale ? (isDesktop ? 70 : 0) : 0,
           bottom: 0,
           left: isRtlLocale ? (0) : (isDesktop ? 70 : 0),
-          child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            padding: EdgeInsets.only(top: isDesktop ? 100 : 60),
-            child: Column(
-              crossAxisAlignment: isDesktop
-                  ? CrossAxisAlignment.start
-                  : CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'aboutMe'.tr(),
-                  style: Theme.of(context).textTheme.headline1?.copyWith(
-                        fontSize: isDesktop ? 40 : 35,
-                      ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Container(
-                  width: 400,
-                  height: 2,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width *
-                      (isDesktop ? 0.6 : 0.8),
-                  child: Text(
-                    UserData.getMyData().description,
-                    textAlign: TextAlign.start,
-                    style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                          fontSize: isDesktop ? 25 : 20,
+          child: Animate(
+            effects: AboutMeTab.isFirstRun
+                ? [
+                    FadeEffect(
+                      duration: 1500.milliseconds,
+                    ),
+                    ShimmerEffect(
+                      duration: 2000.milliseconds,
+                      delay: 1200.milliseconds,
+                      color: Theme.of(context)
+                          .colorScheme
+                          .primary
+                          .withOpacity(0.5),
+                    ),
+                  ]
+                : [],
+            onComplete: (c) {
+              _firstRun();
+            },
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              padding: EdgeInsets.only(top: isDesktop ? 100 : 60),
+              child: Column(
+                crossAxisAlignment: isDesktop
+                    ? CrossAxisAlignment.start
+                    : CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'aboutMe'.tr(),
+                    style: Theme.of(context).textTheme.headline1?.copyWith(
+                          fontSize: isDesktop ? 40 : 35,
                         ),
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    width: 400,
+                    height: 2,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width *
+                        (isDesktop ? 0.6 : 0.8),
+                    child: Text(
+                      UserData.getMyData().description,
+                      textAlign: TextAlign.start,
+                      style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontSize: isDesktop ? 25 : 20,
+                          ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
